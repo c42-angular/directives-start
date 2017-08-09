619fc62f54618c22131fe148546f310b4c57541f
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2017-08-09 07:39:51 +0100

    Structural directive & ngSwitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from 'app/directives/basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from 'app/directives/better-highlight/better-highlight.directive';
+import { UnlessDirective } from './structural-directives/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
